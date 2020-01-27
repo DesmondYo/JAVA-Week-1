d2d81255ac6e7dba4616210239ccbaa364d464f5
Dtown <Dtown@localhost>
2020-01-27 15:57:02 -0700

    App

---
+++ App/src/App.java
@@ -0,0 +1,34 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int age = 21;
+double money = 100.10;
+
+System.out.println(age);
+System.out.println(money);
+
+String firstName = "Desmond";
+String middleIntial = "A";
+String lastName = "Young";
+String fullName = firstName + " " + middleIntial + " " + lastName; //concatenation
+System.out.println(fullName);
+
+double cash = 100-50;
+System.out.println(cash);
+
+
+		
+System.out.println("I am 21 years old");
+
+System.out.println("I have 100.1 in my wallet");
+
+System.out.println("My name is Desmond A Young");
+
+System.out.println("I have 50.00 dollars left in my account after going shopping");
+
+
+	}
+
+}
