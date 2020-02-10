1a291248825939753189854becffa548561b3a1a
Desmond Young <60267316+DesmondYo@users.noreply.github.com>
2020-02-10 16:09:09 -0700

    Update App.java

--- App/src/App.java
+++ App/src/App.java
@@ -4,97 +4,97 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-double Item = 75.15;
-double price = 25.15;
-System.out.println(Item);
-System.out.println(price);
+		double Item = 75.15;
+		double price = 25.15;
+		System.out.println(Item);
+		System.out.println(price);
 
-double money = 100.10;
-double wallet = 1000;
-System.out.println(money);
-System.out.println(wallet);
+		double money = 100.10;
+		double wallet = 1000;
+		System.out.println(money);
+		System.out.println(wallet);
 
-float number = 15;
-float friends = 20;
-System.out.println(number);
-System.out.println(friends);
+		float number = 15;
+		float friends = 20;
+		System.out.println(number);
+		System.out.println(friends);
 
-int age = 20;
-int years = 50;
-System.out.println(age);
-System.out.println(years);
+		int age = 20;
+		int years = 50;
+		System.out.println(age);
+		System.out.println(years);
 
-String name = "Nick";
-String first = "Jay'lynn";
-System.out.println(name);
-System.out.println(first);
+		String name = "Nick";
+		String first = "Jay'lynn";
+		System.out.println(name);
+		System.out.println(first);
 
-String last = "Young";
-String cognomen = "Bynoe";
-System.out.println(last);
-System.out.println(cognomen);
+		String last = "Young";
+		String cognomen = "Bynoe";
+		System.out.println(last);
+		System.out.println(cognomen);
 
-String middle = "Anthony";
-String intial = "Samone";
-System.out.println(middle);
-System.out.println(intial);
+		String middle = "Anthony";
+		String intial = "Samone";
+		System.out.println(middle);
+		System.out.println(intial);
 
-double amountOfMoneyInWallet = 100-50;
-System.out.println(amountOfMoneyInWallet);
+		double amountOfMoneyInWallet = 100-50;
+		System.out.println(amountOfMoneyInWallet);
 
-int totalNumberOfFriends = 210;
-int lifeTime = 21;
-int freindsPerYear= totalNumberOfFriends/lifeTime ;
-System.out.println(totalNumberOfFriends);
-System.out.println(lifeTime);
-System.out.println(freindsPerYear);
-
-
-String firstName = "Desmond";
-String middleIntial = "A";
-String lastName = "Young";
-String fullName = firstName + " " + middleIntial + " " + lastName; //concatenation
-System.out.println(fullName);
+		int totalNumberOfFriends = 210;
+		int lifeTime = 21;
+		int freindsPerYear= totalNumberOfFriends/lifeTime ;
+		System.out.println(totalNumberOfFriends);
+		System.out.println(lifeTime);
+		System.out.println(freindsPerYear);
 
 
+		String firstName = "Desmond";
+		String middleIntial = "A";
+		String lastName = "Young";
+		String fullName = firstName + " " + middleIntial + " " + lastName; //concatenation
+		System.out.println(fullName);
 
-System.out.println("This is the price "  + Item);
 
-System.out.println("This item cost " + price);
 
-System.out.println("I have " + money + " in my wallet");
+		System.out.println("This is the price "  + Item);
 
-System.out.println("I have " + wallet );
+		System.out.println("This item cost " + price);
 
-System.out.println("I have " + number + " freinds");
+		System.out.println("I have " + money + " in my wallet");
 
-System.out.println("I lost " + friends + " friends" );
+		System.out.println("I have " + wallet );
 
-System.out.println("I am " + age + " years old");
+		System.out.println("I have " + number + " freinds");
 
-System.out.println("my age is " + years + " years old" );
+		System.out.println("I lost " + friends + " friends" );
+		
+		System.out.println("I am " + age + " years old");
 
-System.out.println("My first name is " + name);
+		System.out.println("my age is " + years + " years old" );
 
-System.out.println("My father named me " + first);
+		System.out.println("My first name is " + name);
 
-System.out.println("My last name is " + last );
+		System.out.println("My father named me " + first);
 
-System.out.println("My last name " + cognomen + " is famous");
+		System.out.println("My last name is " + last );
 
-System.out.println("My fathers middle name is " + middle);
+		System.out.println("My last name " + cognomen + " is famous");
 
-System.out.println("My wife has a famous" + " middle intial" + " it is " + intial );
+		System.out.println("My fathers middle name is " + middle);
 
-System.out.println("I have " + amountOfMoneyInWallet + " left in my account after going shopping");
+		System.out.println("My wife has a famous" + " middle intial" + " it is " + intial );
 
-System.out.println("The amount of friends I have are " + totalNumberOfFriends );
+		System.out.println("I have " + amountOfMoneyInWallet + " left in my account after going shopping");
 
-System.out.println("I have been on this earth for " + lifeTime + " years of my life");
+		System.out.println("The amount of friends I have are " + totalNumberOfFriends );
 
-System.out.println("I have made " + freindsPerYear + " friends each year since I was born" );
+		System.out.println("I have been on this earth for " + lifeTime + " years of my life");
 
-System.out.println("My full name is " + fullName);
+		System.out.println("I have made " + freindsPerYear + " friends each year since I was born" );
+
+		System.out.println("My full name is " + fullName);
 
 
 
